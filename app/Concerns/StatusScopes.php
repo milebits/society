a6934de0d15c264ead50ab37fcdcd38f5d0578f7
Hicham SADDEK <h.saddek@serrahub.com>
2021-01-21 17:43:49 +0100

    Adding Scopes to the FriendRequest Model

---
+++ app/Concerns/StatusScopes.php
@@ -0,0 +1,102 @@
+<?php
+
+
+namespace Milebits\Society\Concerns;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use function Milebits\Society\Helpers\constVal;
+
+/**
+ * Trait StatusScopes
+ * @package Milebits\Society\Concerns
+ * @mixin Model
+ */
+trait StatusScopes
+{
+    public static function bootStatusScopes()
+    {
+
+    }
+
+
+    public function initializeStatusScopes()
+    {
+        $this->mergeFillable([$this->getStatusColumn()]);
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatusColumn(): string
+    {
+        return constVal($this, 'STATUS_COLUMN', 'status');
+    }
+
+    /**
+     * @return string
+     */
+    public function getQualifiedStatusColumn(): string
+    {
+        return $this->qualifyColumn($this->getStatusColumn());
+    }
+
+    /**
+     * @param Builder $builder
+     * @return string
+     */
+    public function decideStatusColumn(Builder $builder): string
+    {
+        return count((array)(property_exists($builder, 'joins') ? $builder->joins : [])) > 0
+            ? $this->getQualifiedStatusColumn()
+            : $this->getStatusColumn();
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $status
+     * @return Builder
+     */
+    public function scopeWhereStatus(Builder $builder, string $status): Builder
+    {
+        return $builder->where(function (Builder $builder) use ($status): Builder {
+            return $builder->where($this->decideStatusColumn($builder), $status);
+        });
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $status
+     * @return Builder
+     */
+    public function scopeOrWhereStatus(Builder $builder, string $status): Builder
+    {
+        return $builder->orWhere(function (Builder $builder) use ($status): Builder {
+            return $builder->where($this->decideStatusColumn($builder), $status);
+        });
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $status
+     * @return Builder
+     */
+    public function scopeWhereNotStatus(Builder $builder, string $status): Builder
+    {
+        return $builder->where(function (Builder $builder) use ($status): Builder {
+            return $builder->where($this->decideStatusColumn($builder), '!=', $status);
+        });
+    }
+
+    /**
+     * @param Builder $builder
+     * @param string $status
+     * @return Builder
+     */
+    public function scopeOrWhereNotStatus(Builder $builder, string $status): Builder
+    {
+        return $builder->orWhere(function (Builder $builder) use ($status): Builder {
+            return $builder->where($this->decideStatusColumn($builder), '!=', $status);
+        });
+    }
+}
---
+++ helpers/functions.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Milebits\Society\Helpers;
+
+if (!function_exists('constExists')) {
+    /**
+     * @param $class
+     * @param string $constant
+     * @return bool
+     */
+    function constExists($class, string $constant)
+    {
+        if (is_object($class)) $class = get_class($class);
+        return defined(sprintf("%s::%s", $class, $constant));
+    }
+}
+
+if (!function_exists('propVal')) {
+    /**
+     * @param $class
+     * @param string $name
+     * @param null $default
+     * @return mixed|null
+     */
+    function propVal($class, string $name, $default = null)
+    {
+        if (is_object($class)) $class = get_class($class);
+        return $class::$$name ?? $default;
+    }
+}
+
+if (!function_exists('constVal')) {
+    /**
+     * @param $class
+     * @param string $constant
+     * @param null $default
+     * @return mixed|null
+     */
+    function constVal($class, string $constant, $default = null)
+    {
+        if (!propVal($class, $constant)) return $default;
+        return constant(sprintf("%s::%s", $class, $constant)) ?? $default;
+    }
+}
