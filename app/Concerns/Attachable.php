2f21b3925bb695904ef20869f2221074553ed877
Hicham SADDEK <h.saddek@serrahub.com>
2021-01-21 21:55:25 +0100

    Adding stream feature for Attachments inside the model.

--- app/Concerns/Attachable.php
+++ app/Concerns/Attachable.php
@@ -4,8 +4,13 @@
 namespace Milebits\Society\Concerns;
 
 use App\Models\Attachment;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use function Milebits\LaravelStream\Helpers\videoStream;
 
 /**
  * Trait Attachable
@@ -29,5 +34,13 @@ trait Attachable
     public function addAttachment(string $path)
     {
         return $this->attachments()->create(['path' => $path]);
+    }
+
+    /**
+     * @return Application|ResponseFactory|Response|StreamedResponse
+     */
+    public function stream()
+    {
+        return videoStream($this->path);
     }
 }
