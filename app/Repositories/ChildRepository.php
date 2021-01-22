172e5e8cc9aa8a826d3ce8297fa3ff672654f7ec
Hicham SADDEK <h.saddek@serrahub.com>
2021-01-22 01:11:38 +0100

    Adding child repository to simply extends.

---
+++ app/Repositories/ChildRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Milebits\Society\Repositories;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Milebits\Society\Concerns\Sociable;
+
+class ChildRepository
+{
+    /**
+     * @var Model|Sociable|null
+     */
+    protected ?Model $model = null;
+
+    /**
+     * @var SocietyRepository|null
+     */
+    protected ?SocietyRepository $society = null;
+
+    public function __construct(SocietyRepository $society)
+    {
+        $this->society = $society;
+        $this->model = $this->society->parent();
+    }
+
+    /**
+     * @return Model|Sociable|null
+     */
+    public function model(): ?Model
+    {
+        return $this->model;
+    }
+
+    /**
+     * @return SocietyRepository|null
+     */
+    public function society(): ?SocietyRepository
+    {
+        return $this->society;
+    }
+}
