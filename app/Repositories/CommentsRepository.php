d477c9c502c892c30e434fd679ce5bb0176a7bc5
Hicham SADDEK <h.saddek@serrahub.com>
2021-01-22 01:13:07 +0100

    Adding empty comments repository to improve after.

---
+++ app/Repositories/CommentsRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Milebits\Society\Repositories;
+
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Builder;
+
+class CommentsRepository extends ChildRepository
+{
+    public function delete(Comment $comment): bool
+    {
+
+    }
+
+    public function update(Comment $comment, $data): bool
+    {
+
+    }
+
+    public function add($data): Comment
+    {
+
+    }
+
+    public function all(): Builder
+    {
+
+    }
+
+    public function of(): Builder
+    {
+
+    }
+}
