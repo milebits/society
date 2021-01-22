3725a718a57d5c0d94e2b9b0c9a5f6ca7a64d48e
Hicham SADDEK <h.saddek@serrahub.com>
2021-01-22 04:46:26 +0100

    Updating new scopes to files

---
+++ src/Models/Story.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Milebits\Society\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Milebits\Eloquent\Filters\Concerns\Enableable;
+
+class Story extends Model
+{
+
+    use SoftDeletes, HasFactory, Enableable;
+
+    protected $fillable = ['content'];
+}
