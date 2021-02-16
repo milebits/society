1f361deeb67870d6c45ae8ba4fb8e83760790df9
Hicham SADDEK <h.saddek@serrahub.com>
2021-02-16 16:54:19 +0100

    - Adding `NotBlockedByPersonMiddleware` Middleware to allow users to quickly check if the user has been blocked by another person, therefore not giving them access to a certain request rights.

---
+++ src/Http/Middleware/NotBlockedByPersonMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Milebits\Society\Http\Middleware;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Milebits\Society\Concerns\Sociable;
+use function Milebits\Helpers\Helpers\hasTrait;
+
+class NotBlockedByPersonMiddleware
+{
+    /**
+     * @param Request $request
+     * @param callable $next
+     * @param Model|Sociable $person
+     * @return RedirectResponse|Response
+     */
+    public function handle(Request $request, callable $next, $person)
+    {
+        if (!hasTrait($this->user($request)->getMorphClass(), Sociable::class))
+            return $next($request);
+
+        if (!$this->user($request)->society()->friends()->isBlockedBy($person)) return back();
+        return $next($request);
+    }
+
+    /**
+     * @param Request $request
+     * @return Sociable|Model|mixed
+     */
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
+}
